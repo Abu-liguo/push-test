69ef0bb8b04222eb510604456f0b51f6d84e4f67
liguo <2283513276@qq.com>
2026-09-16 15:11:32 +0800

    second commit

--- src/main/java/com/abu/GitTest.java
+++ src/main/java/com/abu/GitTest.java
@@ -3,5 +3,7 @@ package com.abu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Git");
+
     }
 }
